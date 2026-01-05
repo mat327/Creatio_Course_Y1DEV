ea932469bbd5e3b25f9cabd2b166080420bf52b6
Mateusz Białek <onetuchidol@gmail.com>
2026-01-05 13:18:55 +0100

    Created new business process UsrGetCompanyData and subprocess UsrAddAccount. Registered new web service UsrDNBAccountsCollector.

---
+++ UsrYacht/Schemas/UsrAccountRequestDetails/UsrAccountRequestDetails.js
@@ -0,0 +1,111 @@
+define("UsrAccountRequestDetails", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "PageTitle",
+				"values": {
+					"caption": "#MacrosTemplateString(#ResourceString(PageTitle_caption)#)#",
+					"visible": true,
+					"headingLevel": "label"
+				}
+			},
+			{
+				"operation": "remove",
+				"name": "ContinueInOtherPageButton"
+			},
+			{
+				"operation": "merge",
+				"name": "MainContainer",
+				"values": {
+					"alignItems": "stretch"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "SaveButton",
+				"values": {
+					"caption": "#ResourceString(SaveButton_caption)#"
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "CompanyName",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter1_88il5g1",
+					"control": "$PageParameters_UsrTextParameter1_88il5g1",
+					"placeholder": "",
+					"tooltip": "",
+					"readonly": false,
+					"multiline": false,
+					"labelPosition": "above"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Country",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter2_omp0iy7",
+					"control": "$PageParameters_UsrTextParameter2_omp0iy7",
+					"placeholder": "",
+					"tooltip": "",
+					"readonly": false,
+					"multiline": false,
+					"labelPosition": "above"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PageParameters_UsrTextParameter1_88il5g1": {
+						"modelConfig": {
+							"path": "PageParameters.UsrCompanyName"
+						}
+					},
+					"PageParameters_UsrTextParameter2_omp0iy7": {
+						"modelConfig": {
+							"path": "PageParameters.UsrCountry"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
